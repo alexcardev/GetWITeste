7c2433dd4938ef31b78981c81f3dfd31a18768f9
Alexandre Carvalho <alexandre.carvalho@live.com>
2019-09-06 15:24:41 -0300

    Criação do dos projetos e suas configurações.

---
+++ GetWITeste/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GetWITeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
